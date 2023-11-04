9a14ba3b8990becbaa71512a9a0633caed1e445b
Tyler Holloway <tholloway@THOLLOWAY2-MBP.local>
2023-11-03 21:38:39 -0400

    Successfully search, scrapes, and responds.

---
+++ main.py
@@ -0,0 +1,229 @@
+# The os module provides a way to interface with the underlying operating system that Python is running on –
+# such as accessing the file system.
+import os
+
+# The dotenv package is used to load environment variables from a .env file into the environment.
+from dotenv import load_dotenv
+
+# PromptTemplate is a class from the langchain package, likely used to create templates for text prompts
+# that can be used with language models or other text generation tools.
+from langchain import PromptTemplate
+
+# These imports from langchain.agents are used to initialize an agent and to import the Tool and AgentType classes.
+# An agent here refers to a component that can interact with language models or perform specific tasks in a workflow.
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+
+from langchain.prompts import MessagesPlaceholder
+
+# ChatOpenAI from langchain.chat_models likely represents a chat model based on OpenAI's GPT or similar models.
+from langchain.chat_models import ChatOpenAI
+
+# ChatOpenAI from langchain.chat_models likely represents a chat model based on OpenAI's GPT or similar models.
+from langchain.chat_models import ChatOpenAI
+
+# ConversationSummaryBufferMemory from langchain.memory is likely a class for managing the state and memory of conversations,
+# possibly summarizing them for later use.
+from langchain.memory import ConversationSummaryBufferMemory
+
+# RecursiveCharacterTextSplitter seems to be a utility to split text based on character count, which can be useful
+# for processing large texts that need to fit within certain size constraints.
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# This function from langchain.chains.summarize presumably loads a chain of operations that includes summarization,
+# possibly to condense text input into more digestible content.
+from langchain.chains.summarize import load_summarize_chain
+
+# BaseTool is likely a base class for various tools that can be used within the langchain framework.
+from langchain.tools import BaseTool
+
+# Pydantic's BaseModel and Field are used for data validation and settings management using Python type annotations.
+from pydantic import BaseModel, Field
+
+# The typing module is imported here to make use of Type, which is likely used for Python type hints.
+from typing import Type
+
+# BeautifulSoup from bs4 is a Python library for parsing HTML and XML documents, often used for web scraping.
+from bs4 import BeautifulSoup
+
+# The requests module is used to make HTTP requests in Python, such as GET and POST requests to web servers.
+import requests
+
+# The json module provides an easy way to encode and decode data in JSON.
+import json
+
+# SystemMessage from langchain.schema is probably a class representing a system message within the framework,
+# which could be used for logs, errors, or other system-related notifications.
+from langchain.schema import SystemMessage
+
+# FastAPI is a modern, fast (high-performance) web framework for building APIs with Python 3.7+ based on standard Python type hints.
+from fastapi import FastAPI
+
+import streamlit as st  # Ensure you have this import statement
+
+
+# Load environment variables from a .env file into the system's environment.
+load_dotenv()
+
+# Browserless is a service that provides a browser automation API.
+brwoserless_api_key = os.getenv("BROWSERLESS_API_KEY")
+
+# SERP API is typically used for scraping and analyzing search engine results programmatically.
+serper_api_key = os.getenv("SERP_API_KEY")
+
+def search(query):
+    url = "https://google.serper.dev/search"
+
+    payload = json.dumps({
+        "q": query
+    })
+
+    headers = {
+        'X-API-KEY': serper_api_key,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+    return response.text
+
+def summary(objective, content):
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n"], chunk_size=10000, chunk_overlap=500)
+    
+    docs = text_splitter.create_documents([content])
+    
+    map_prompt = """
+    Write a summary of the following text for {objective}:
+    "{text}"
+    SUMMARY:
+    """
+    map_prompt_template = PromptTemplate(
+        template=map_prompt, input_variables=["text", "objective"])
+
+    summary_chain = load_summarize_chain(
+        llm=llm,
+        chain_type='map_reduce',
+        map_prompt=map_prompt_template,
+        combine_prompt=map_prompt_template,
+        verbose=True
+    )
+
+    output = summary_chain.run(input_documents=docs, objective=objective)
+
+    return output
+
+def scrape_website(objective: str, url: str):
+    print("Scraping website...")
+    # Define the headers for the request
+    headers = {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+    }
+
+    # Define the data to be sent in the request
+    data = {
+        "url": url
+    }
+
+    # Convert Python object to JSON string
+    data_json = json.dumps(data)
+
+    # Send the POST request
+    post_url = f"https://chrome.browserless.io/content?token={brwoserless_api_key}"
+    response = requests.post(post_url, headers=headers, data=data_json)
+
+    # Check the response status code
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        text = soup.get_text()
+
+        if len(text) > 10000:
+            print("Length of text: ", len(text))
+            output = summary(objective, text)
+            return output
+        else:
+            return text
+    else:
+        print(f"HTTP request failed with status code {response.status_code}")
+
+class ScrapeWebsiteInput(BaseModel):
+    """Inputs for scrape_website"""
+    objective: str = Field(
+        description="The objective & task that users give to the agent")
+    url: str = Field(description="The url of the website to be scraped")
+
+
+class ScrapeWebsiteTool(BaseTool):
+    name = "scrape_website"
+    description = "useful when you need to get data from a website url, passing both url and objective to the function; DO NOT make up any url, the url should only be from the search results"
+    args_schema: Type[BaseModel] = ScrapeWebsiteInput
+
+    def _run(self, objective: str, url: str):
+        return scrape_website(objective, url)
+
+    def _arun(self, url: str):
+        raise NotImplementedError("error here")
+
+# 3. Create langchain agent with the tools above
+tools = [
+    Tool(
+        name="Search",
+        func=search,
+        description="useful for when you need to answer questions about current events, data. You should ask targeted questions"
+    ),
+    ScrapeWebsiteTool(),
+]
+
+system_message = SystemMessage(
+    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
+            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
+            
+            Please make sure you complete the objective above with the following rules:
+            1/ You should do enough research to gather as much information as possible about the objective
+            2/ If there are url of relevant links & articles, you will scrape it to gather more information
+            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
+            4/ You should not make things up, you should only write facts & data that you have gathered
+            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
+            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+)
+
+agent_kwargs = {
+    "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+    "system_message": system_message,
+}
+
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+memory = ConversationSummaryBufferMemory(
+    memory_key="memory", return_messages=True, llm=llm, max_token_limit=1000)
+
+agent = initialize_agent(
+    tools,
+    llm,
+    agent=AgentType.OPENAI_FUNCTIONS,
+    verbose=True,
+    agent_kwargs=agent_kwargs,
+    memory=memory,
+)
+
+# 4. Use streamlit to create a web app
+def main():
+    st.set_page_config(page_title="AI research agent", page_icon=":bird:")
+
+    st.header("AI research agent :bird:")
+    query = st.text_input("Research goal")
+
+    if query:
+        st.write("Doing research for ", query)
+
+        result = agent({"input": query})
+
+        st.info(result['output'])
+
+
+if __name__ == '__main__':
+    main()
